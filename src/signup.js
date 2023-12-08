12f417e8e876a46346e2413aade77ef087e17f2a
Shouvik Samadder <122898155+ShouvikSamadder@users.noreply.github.com>
2023-12-08 11:39:58 +0530

    Update signup.js

--- src/signup.js
+++ src/signup.js
@@ -3,6 +3,6 @@ function submitForm() {
     var email = document.getElementById('email').value;
     var dob = document.getElementById('dob').value;
   
-    alert('Account created! Log In now\nName: ' + name + '\nEmail: ' + email + '\nDate of Birth: ' + dob);
+    alert('Account is Created! Log In Now!\nName: ' + name + '\nEmail: ' + email + '\nDate of Birth: ' + dob);
   }
   
